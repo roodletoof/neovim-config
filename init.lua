adc7911093aef1d8bc598f65f3e613c7f24c1659
Ivar Fatland <fatland99@hotmail.com>
2025-06-07 12:40:46 +0200

    use gdb instead of lldb

--- init.lua
+++ init.lua
@@ -468,28 +468,57 @@ require'lazy'.setup{ --{{{1
             dap.adapters.godot = { type = 'server', host = '127.0.0.1', port = 6006, }
             dap.configurations.gdscript = { {type = 'godot', request = 'launch', name = 'Launch scene', project = "${workspaceFolder}",} }
 
-            dap.adapters.lldb = {
-                type = 'executable',
-                command = vim.fn.exepath('lldb-dap'),
-                name = 'lldb'
+            dap.adapters.gdb = {
+            type = "executable",
+            command = "gdb",
+            args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
             }
 
             dap.configurations.c = {
-                {
-                    name = 'Launch',
-                    type = 'lldb',
-                    request = 'launch',
-                    program = function()
-                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-                    end,
-                    cwd = '${workspaceFolder}',
-                    stopOnEntry = false,
-                    args = function()
-                        local i = vim.fn.input('input args: ')
-                        return vim.fn.split(i)
-                    end,
-                    runInTerminal = true,
-                },
+            {
+                name = "Launch this file",
+                type = "gdb",
+                request = "launch",
+                program = function ()
+                    local filename = vim.fn.expand("%:t")  -- Get the current file name (e.g., "main.c")
+                    return filename:gsub("%.c$", ".o")     -- Replace ".c" at the end with ".o"
+                end ,
+                cwd = "${workspaceFolder}",
+                stopAtBeginningOfMainSubprogram = false,
+            },
+            {
+                name = "Launch",
+                type = "gdb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = "${workspaceFolder}",
+                stopAtBeginningOfMainSubprogram = false,
+            },
+            {
+                name = "Select and attach to process",
+                type = "gdb",
+                request = "attach",
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                pid = function()
+                local name = vim.fn.input('Executable name (filter): ')
+                    return require("dap.utils").pick_process({ filter = name })
+                end,
+                cwd = '${workspaceFolder}'
+            },
+            {
+                name = 'Attach to gdbserver :1234',
+                type = 'gdb',
+                request = 'attach',
+                target = 'localhost:1234',
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = '${workspaceFolder}'
+            },
             }
             dap.configurations.cpp = dap.configurations.c
             dap.configurations.rust = dap.configurations.c
